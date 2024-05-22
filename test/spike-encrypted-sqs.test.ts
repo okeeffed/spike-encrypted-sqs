b9d3b52cf0a1693e7a7ac6eb2640e7a2dd639481
Dennis O'Keeffe <hello@dennisokeeffe.com>
2024-05-22 15:16:11 +1000

    Initial commit

---
+++ test/spike-encrypted-sqs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SpikeEncryptedSqs from '../lib/spike-encrypted-sqs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/spike-encrypted-sqs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SpikeEncryptedSqs.SpikeEncryptedSqsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
